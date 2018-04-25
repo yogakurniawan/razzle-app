d667b7fd13f59f809ac9ce0f6170c4ed97ef3cea
Yoga Kurniawan <yoga.kwn@gmail.com>
2018-04-25 18:56:46 +0700

    add firebase stuffs; add spinner component;

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,37 +1,22 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Scrollbars } from 'react-custom-scrollbars'
-import Dropdown from 'react-styled-dropdown'
 import { Button } from 'components/Button'
+// import Spinner from 'components/Spinner'
+import Circular from 'components/Spinner/Circular'
 import Page from 'components/HOC/Page'
 
 class Home extends Component {
   render() {
-    const options = [
-      { value: 'one', label: 'One' },
-      { value: 'two', label: 'Two', className: 'myOptionClassName' },
-      {
-       type: 'group', name: 'group1', items: [
-         { value: 'three', label: 'Three', className: 'myOptionClassName' },
-         { value: 'four', label: 'Four' }
-       ]
-      },
-      {
-       type: 'group', name: 'group2', items: [
-         { value: 'five', label: 'Five' },
-         { value: 'six', label: 'Six' }
-       ]
-      }
-    ]
-    const defaultOption = options[0]
     return (
       <div>
         <ul>
           <li><Link to="/sandwiches">Sandwiches</Link></li>
           <li><Link to="/tacos">Tacos</Link></li>
         </ul>
-        <Button color="primary" outline>Themed</Button>
-        <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
+        <Button color="primary">
+          <Circular size="20px" color="#fff" />
+        </Button>
         <Scrollbars
           style={{ height: 300 }}>
           <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyAOrmzsApwFAYSDJ9lIbsUT6ZiOHAZipwM",
+  authDomain: "radiant-fire-8622.firebaseapp.com",
+  databaseURL: "https://radiant-fire-8622.firebaseio.com",
+  projectId: "radiant-fire-8622",
+  storageBucket: "radiant-fire-8622.appspot.com",
+  messagingSenderId: "443846187284"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+const db = firebase.database();
+const auth = firebase.auth();
+
+export { db, auth };
